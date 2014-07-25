62a73fef33c865dc0c3f7e78c502c9efe5cd61eb
Suitcaser <nkviz07@gmail.com>
2014-07-25 20:10:02 +0400

    no-hosts-bug-fix;

--- InternationalServerHoNPatcher/Program.cs
+++ InternationalServerHoNPatcher/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Security.Principal;
 using System.IO;
 using System.Diagnostics;
 
@@ -15,39 +11,29 @@ namespace InternationalServerHoNPatcher
         {
             string filePath = Environment.GetEnvironmentVariable("SystemRoot") + @"\system32\drivers\etc\hosts";
             string path = Environment.GetEnvironmentVariable("SystemRoot") + @"\system32\drivers\etc";
-            AppDomain.CurrentDomain.SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
-            WindowsPrincipal currentPrincipal = (WindowsPrincipal)Thread.CurrentPrincipal;
+            bool doesHostsExist = File.Exists(filePath);
+
 
             List<string> result = new List<string>();
-            foreach (var str in System.IO.File.ReadAllLines(filePath))
-            {
-                if (!(str.Contains("masterserver.hon.s2games.com") || str.Contains("masterserver.naeu.heroesofnewerth.com") || str.StartsWith("#Patched hosts file")))
+            if (doesHostsExist)
+                foreach (var str in System.IO.File.ReadAllLines(filePath))
                 {
-                    result.Add(str.Trim());
+                    if (!(str.Contains("masterserver.hon.s2games.com") || str.Contains("masterserver.naeu.heroesofnewerth.com") || str.StartsWith("# Patched hosts file")))
+                    {
+                        result.Add(str.Trim());
+                    }
                 }
-            }
             result.Add("104.131.237.217 masterserver.naeu.heroesofnewerth.com");
             result.Add("104.131.237.217 masterserver.hon.s2games.com");
-
-            //System.Diagnostics.Process process = new System.Diagnostics.Process();
-            //System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            //startInfo.FileName = "cmd.exe";
-            //startInfo.Arguments = "echo 0.0.0.0 websitename.com >> %WINDIR%\\System32\\Drivers\\Etc\\Hosts";
-            //process.StartInfo = startInfo;
-            //process.Start();
-
-            //ExecuteCommandAsAdmin(@"/c attrib -R -S -H hosts",path);
-            ////ExecuteCommandAsAdmin(@"/c echo 0.0.0.0 websitename.com >> D:\LOL.txt");
-            //string comm = @"/c echo " + string.Join("\n",result) + @" > " + filePath;
-            //ExecuteCommandAsAdmin(comm, path);
-            //ExecuteCommandAsAdmin(@"/c attrib +S hosts",path);
 
             var batContents = @"@echo off" + Environment.NewLine +
                 @"cd /d ""%windir%\system32\drivers""" + Environment.NewLine +
                 @"cd etc" + Environment.NewLine +
-                @"attrib -R -S -H hosts" + Environment.NewLine +
-                @"echo #Patched hosts file > hosts" + Environment.NewLine +
+                    (doesHostsExist ?
+                    @"echo. 2>hosts" :
+                    @"attrib -R -S -H hosts") 
+                    + Environment.NewLine +
+                @"echo # Patched hosts file > hosts" + Environment.NewLine +
                 @"echo " + string.Join(@" >> hosts" + Environment.NewLine + @"echo ", result) + @" >> hosts" + Environment.NewLine + 
                 @"attrib +R +S hosts" + Environment.NewLine +
                 @"ipconfig /flushdns" + Environment.NewLine +
@@ -60,7 +46,8 @@ namespace InternationalServerHoNPatcher
                 file.Write(batContents);
             }
 
-            System.Diagnostics.Process.Start(batPath);
+            //System.Diagnostics.Process.Start(batPath);
+            ExecuteCommandAsAdmin(batPath);
 
             File.Delete(batPath);
         }
